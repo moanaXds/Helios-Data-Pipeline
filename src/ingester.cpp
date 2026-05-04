5de26bcaa74fe5d2d5f1d0cb32c2d2aa48d9a1c2
zain <i242609@isb.nu.edu.pk>
2026-05-04 07:46:22 +0500

    Read input directory

--- src/ingester.cpp
+++ src/ingester.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
 #include <unistd.h>
+#include <dirent.h>
 
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
     cout << "[INGESTER PID: " << getpid() << endl;
     cout << "[INGESTER PPID: " << getppid() << endl;
+    
+    // open the input directory and list all the files
+    if (argc != 2) {
+        cout << "usage: "<< argv[0] << " <input_directory_path>" << endl;
+        return 1;
+    } 
+
+    DIR* dir = opendir(argv[1]);
+    if (dir == NULL) {
+        cout << "error while opening directory:" << argv[1] <<endl;
+        return 1;
+    }
+    struct dirent* entry;
+    cout << "files in the input directory are:"<< endl;
+    while((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_REG) { // only print regular files
+            cout << entry->d_name << endl;
+        }
+    }
+    closedir(dir);
+
+
 
     sleep(2);
 
