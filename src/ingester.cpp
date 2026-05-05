db4fbb4d4fa3f3cb81b8804920e61dc525e5e210
Zain <i242609@isb.nu.edu.pk>
2026-05-05 09:32:49 +0500

    write csv file data into pipe

--- src/ingester.cpp
+++ src/ingester.cpp
@@ -3,13 +3,18 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <cerrno>
-
+#include <fcntl.h>
+#include <vector>
+#include <string.h>
 using namespace std;
 
 int main(int argc, char* argv[]) {
     cout << "[INGESTER PID: " << getpid() << endl;
     cout << "[INGESTER PPID: " << getppid() << endl;
     
+    // store the file names
+    vector<string> csv_file_names;
+
     // open the input directory and list all the files
     if (argc != 2) {
         cout << "usage: "<< argv[0] << " <input_directory_path>" << endl;
@@ -24,8 +29,11 @@ int main(int argc, char* argv[]) {
     struct dirent* entry;
     cout << "files in the input directory are:"<< endl;
     while((entry = readdir(dir)) != NULL) {
-        if (entry->d_type == DT_REG) { // only print regular files
-            cout << entry->d_name << endl;
+        if (entry->d_type == DT_REG)  { 
+        string name = entry->d_name;
+        if (name.size() >= 4 && name.substr(name.size() - 4) == ".csv") {
+            csv_file_names.push_back(string(argv[1]) + "/" + name);
+        }
         }
     }
     closedir(dir);
@@ -45,10 +53,30 @@ int main(int argc, char* argv[]) {
     } else {
         std::cout << "Pipe created successfully." << std::endl;
     }
+    
+    int fifo_write = open(pipePath,O_WRONLY);
+    if (fifo_write <0)
+    {
+        cout << "Error opening file" << endl;
+    }
+    else
+    {
+        char buffer[1024];  // line buffer
+
+        for (const string& filepath : csv_file_names) {
+            FILE* file = fopen(filepath.c_str(), "r");
+            if (file == NULL) { /* handle error */ continue; }
+            
+            while (fgets(buffer, sizeof(buffer), file) != NULL) {
+                write(fifo_write, buffer, strlen(buffer));
+                
+            }
+            fclose(file);
+        }
+    }
+    close(fifo_write);
 
 
-
-    sleep(2);
 
     cout << "[INGESTER finished work" << endl;
 
